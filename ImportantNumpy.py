7c10028f5140e5c7419245b6c9fb7bc91e0f5c2b
Abhilash V Gopal <abhilash1392@gmail.com>
2021-06-24 12:57:33 +0530

    Create ImportantNumpy.py

---
+++ ImportantNumpy.py
@@ -0,0 +1,13 @@
+# Importing the numpy library
+import numpy as np
+
+# Dot product
+A.dot(B)
+
+# Cumulative sum
+a.cumsum(axis=1) ## For sum of each row
+a.cumsum(axis=0) ## For sum of each column
+
+rg = np.random.default_rng(1)  # create instance of default random number generator
+
+b.sum(axis=0)     # sum of each column
